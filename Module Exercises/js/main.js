1757107db12c373672387fa833837ac935c3914e
ChakmaKanak174 <kanakchakma195@gmail.com>
2025-02-10 09:06:03 +0200

    Javascript part of the FAQ is done

---
+++ Module Exercises/js/main.js
@@ -0,0 +1,52 @@
+// Faq Accordion
+
+document.addEventListener('DOMContentLoaded', () => {
+    const faqContainer = document.querySelector('.faq-content'); //queryselector selects the class name from the html conent classes.
+
+
+    //Here e is the event click and .target returns the class name of the item clicked
+    faqContainer.addEventListener('click', (e) => {
+        const groupHeader = e.target.closest('.faq-group-header');
+        if(!groupHeader) return;
+
+        //The line below brings the parent class of the groupHeader
+        const group = groupHeader.parentElement;
+
+
+        const groupBody = group.querySelector('.faq-group-body')
+        const icon = groupHeader.querySelector('i')
+
+        //toggle icon
+
+        icon.classList.toggle('fa-plus')
+        icon.classList.toggle('fa-minus')
+
+        //Toggle visibility of body
+
+        groupBody.classList.toggle('open')
+
+        //Close other faq bodies when I click on a new faq
+
+        const otherGroups = faqContainer.querySelectorAll('.faq-group')
+        
+        otherGroups.forEach( (otherGroup) => {
+            
+            
+            if(otherGroup != group) {
+
+                const otherGrupBody = otherGroup.querySelector('.faq-group-body')
+                
+                // inside the query selector i can write the class names just like I can write in the CSS file
+                const otherIcon = otherGroup.querySelector('.faq-group-header i')
+
+
+                otherGrupBody.classList.remove('open')
+                // Here toogle doesnt work because toggle works for both removing and adding. 
+                otherIcon.classList.remove('fa-minus')
+                otherIcon.classList.add('fa-plus')
+            }
+
+
+        });
+    })
+})
